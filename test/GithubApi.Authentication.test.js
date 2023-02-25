abcc9458e728cad3c24b40a663a902f34555a07c
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2023-02-25 16:30:05 -0500

    La tercera es la vencida

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -11,13 +11,13 @@ describe('Github Api Test', () => {
     it('Via OAuth2 Tokens by Header', async () => {
       const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repository}`, {
         headers: {
-          Authorization: `token ${process.env.ACCESS_TOKEN}`
+          Authorization: `${process.env.ACCESS_TOKEN}`
         }
       });
-
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
+
     it('Via OAuth2 Tokens by parameter', async () => {
       const response = await axios.get(
         `${urlBase}/repos/${githubUserName}/${repository}`,
