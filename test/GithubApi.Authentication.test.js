8147eb8c5e74779e9f19bcbc486d5b459f6b71ad
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2023-02-25 15:41:53 -0500

    pruebita

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -7,13 +7,14 @@ const axios = require('axios');
 const urlBase = 'https://api.github.com';
 const githubUserName = 'JuanFelipeJaramillo20';
 const repository = 'workshop-api-testing-js';
+const ACCESS_TOKEN = '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';
 
 describe('Github Api Test', () => {
   describe('Authentication', () => {
     it('Via OAuth2 Tokens by Header', async () => {
       const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repository}`, {
         headers: {
-          Authorization: `token ${process.env.ACCESS_TOKEN}`
+          Authorization: `token ${ACCESS_TOKEN}`
         }
       });
 
